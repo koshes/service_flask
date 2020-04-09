d88ce2b2acfe28dfb5b9aafc69a329cab28aaf2e
Note_F <ysvolodin@gmail.com>
2020-04-09 15:55:54 +0300

    Add Flask-Migration and user registration

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,10 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
+from webapp.user.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('User name', validators=[DataRequired()], render_kw={"class": "form-control"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={"class": "form-control"})
     remember_me = BooleanField('Remember me', default=True, render_kw={"class": "form-check-input"})
     submit = SubmitField('Submit!', render_kw={"class": "btn-primary"})
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('User name', validators=[DataRequired()], render_kw={"class": "form-control"})
+    email = StringField('Emaii', validators=[DataRequired(), Email()], render_kw={"class": "form-control"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"class": "form-control"})
+    password2 = PasswordField('Repeat the password', validators=[DataRequired(), EqualTo('password')], render_kw={"class": "form-control"})
+    submit = SubmitField('Submit!', render_kw={"class": "btn-primary"})
+
+    def validate_username(self, username):
+        user_count = User.query.filter_by(username=username.data).count()
+        if user_count > 0:
+            raise ValidationError('User with the same name already exists!')
+
+    def validate_email(self, email):
+        email_count = User.query.filter_by(email=email.data).count()
+        if email_count > 0:
+            raise ValidationError('User with the same email already exists!')
+
--- webapp/user/models.py
+++ webapp/user/models.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(128))
+    email = db.Column(db.String(50))
     role = db.Column(db.String(10), index=True)
 
     def set_password(self, password):
