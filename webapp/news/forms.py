067ccbb9ea7057dfe4ff62ba044d440c3a54080a
Yakov_V <ysvolodin@gmail.com>
2020-05-04 12:14:46 +0300

    Add Comment form for news and checks for users

---
+++ webapp/news/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import HiddenField, StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+
+from webapp.news.models import News
+
+
+class CommentForm(FlaskForm):
+    news_id = HiddenField('ID news', validators=[DataRequired()])
+    comment_text = StringField('Comment text', validators=[DataRequired()], render_kw={'class': 'form-control'})
+    submit = SubmitField('Submit!', render_kw={'class': 'btn btn-primary'})
+
+
+    def validate_news_id(self, news_id):
+        if not News.query.get(news_id.data):
+            raise ValidationError('News with such ID doesn\'t exist!')
+
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_user, logout_user
 from webapp import db
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
 
@@ -11,7 +12,7 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target())
     title = 'Authorization'
     login_form = LoginForm()
     return render_template("user/login.html", page_title=title, form=login_form)
@@ -27,7 +28,7 @@ def process_login():
             login_user(user, remember=form.remember_me.data)
             login_user(user)
             flash('Authorization complete!')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())
 
     flash('Wrong name or password!')
     return redirect(url_for('user.login'))
